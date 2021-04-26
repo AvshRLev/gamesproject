d4df97b002815c0cb2b1e3eaaf7a3228bb8842b4
AvshRLev <avsha.earth@gmail.com>
2021-04-26 10:19:50 +0300

    created first 4 levels

--- games/static/games/proto.js
+++ games/static/games/proto.js
@@ -21,15 +21,15 @@ class Game {
   }
 
   scoreDisplayUpdate() {
-    this.scoreDisplay.innerHTML = this.score.toLocaleString('en-US', {
+    this.scoreDisplay.innerHTML = this.score.toLocaleString("en-US", {
       minimumIntegerDigits: 4,
-      useGrouping: false
+      useGrouping: false,
     });
   }
 
   scoreReset() {
     this.score = zero();
-    this.scoreDisplayUpdate()
+    this.scoreDisplayUpdate();
   }
 
   timerRun() {
@@ -43,16 +43,16 @@ class Game {
     this.timeDisplayUpdate();
   }
   timeDisplayUpdate() {
-    this.timeDisplay.innerHTML = this.time.toLocaleString('en-US', {
+    this.timeDisplay.innerHTML = this.time.toLocaleString("en-US", {
       minimumIntegerDigits: 2,
-      useGrouping: false
+      useGrouping: false,
     });
   }
-  
+
   announceLose(gameId) {
-    this.grid.innerHTML = '';
-    this.grid.classList.remove('youwin');
-    this.grid.classList.add('youlose');
+    this.grid.innerHTML = "";
+    this.grid.classList.remove("youwin");
+    this.grid.classList.add("youlose");
     clearInterval(gameId);
     this.gamePause();
   }
@@ -61,31 +61,31 @@ class Game {
     let timerId = 0;
     let scoreId = 0;
     scoreId = setInterval(() => {
-      this.keepScore(board)
-    }, 100)
+      this.keepScore(board);
+    }, 100);
     timerId = setInterval(() => {
       this.timerRun();
-    }, 1000)
+    }, 1000);
     let startButtonEventHandler = () => {
       if (gameId) {
         clearInterval(gameId);
         gameId = zero();
         this.gamePause();
-        return 
-      } 
+        return;
+      }
       this.gameRun();
       gameId = setInterval(() => {
-        if (allAliensDeadOn(board)){
-          this.announceWin(gameId)
-          return           
-        } 
+        if (allAliensDeadOn(board)) {
+          this.announceWin(gameId);
+          return;
+        }
         if (aliensHitGround(board)) {
-          this.announceLose(gameId)
-          return 
+          this.announceLose(gameId);
+          return;
         }
         board.moveAliens();
       }, 500);
-    }
+    };
     let restartButtonEventHandler = () => {
       clearInterval(gameId);
       clearInterval(timerId);
@@ -93,31 +93,34 @@ class Game {
       this.timerReset();
       this.scoreReset();
       this.restart(startButtonEventHandler, restartButtonEventHandler);
-    }
+    };
     let board = new Board(this.grid, BOARD_WIDTH, this.setup);
-    this.startButton.addEventListener("mousedown", startButtonEventHandler)
+    this.startButton.addEventListener("mousedown", startButtonEventHandler);
     board.draw(this.setup);
     this.controls(board);
-    this.restartButton.addEventListener("mousedown", restartButtonEventHandler)
+    this.restartButton.addEventListener("mousedown", restartButtonEventHandler);
   }
   announceWin(gameId) {
-    this.grid.innerHTML = '';
-    this.grid.classList.remove('youlose');
-    this.grid.classList.add('youwin');
+    this.grid.innerHTML = "";
+    this.grid.classList.remove("youlose");
+    this.grid.classList.add("youwin");
     clearInterval(gameId);
     this.gamePause();
   }
   restart(startButtonEventHandler, restartButtonEventHandler) {
     this.startButton.removeEventListener("mousedown", startButtonEventHandler);
-    this.restartButton.removeEventListener("mousedown", restartButtonEventHandler);
+    this.restartButton.removeEventListener(
+      "mousedown",
+      restartButtonEventHandler
+    );
     this.cleanBoard();
     this.gamePause();
     this.start();
   }
   cleanBoard() {
-    this.grid.innerHTML = '';
+    this.grid.innerHTML = "";
   }
-  gamePause(){
+  gamePause() {
     this.isRunning = false;
   }
   gameRun() {
@@ -141,14 +144,14 @@ class Game {
 }
 
 function allAliensDeadOn(board) {
-  return !board.containsAliens()
+  return !board.containsAliens();
 }
 
 function aliensHitGround(board) {
-  return board.squares.some(square => square.isGround() && square.hasAlien())
+  return board.squares.some((square) => square.isGround() && square.hasAlien());
 }
 function addOneTo(time) {
-  return time + 1
+  return time + 1;
 }
 
 class Setup {
@@ -185,7 +188,7 @@ class Square {
   isGround() {
     return this.borders.includes("ground");
   }
-  
+
   isTop() {
     return this.index < this.lineWidth;
   }
@@ -200,7 +203,7 @@ class Square {
   setEmpty() {
     this.div.className = "";
     this.character = null;
-    this.type = null
+    this.type = null;
   }
 
   setGround() {
@@ -216,7 +219,7 @@ class Square {
   }
 
   hasDefender() {
-    return this.type === "defender" ;
+    return this.type === "defender";
   }
   hasRocket() {
     return this.type === "rocket";
@@ -307,7 +310,10 @@ class Board {
     this.alienLocations = Object.keys(this.setupObject).map(Number);
     this.initialAlienLocations = this.alienLocations;
     this.alienHealthLevels = Object.values(this.setupObject);
-    this.initialTotalAlienHealth = this.alienHealthLevels.reduce((a,b) => a + b, 0);
+    this.initialTotalAlienHealth = this.alienHealthLevels.reduce(
+      (a, b) => a + b,
+      0
+    );
     this.currentTotalAlienHealth = this.initialTotalAlienHealth;
     this.direction = DIRECTION_RIGHT;
     this.previousDirection = DIRECTION_RIGHT;
@@ -332,7 +338,7 @@ class Board {
     this.squares.push(new Square(index, div, this.width, borders));
   }
   twoBottomRows() {
-    return this.width * this.width - 2*this.width;
+    return this.width * this.width - 2 * this.width;
   }
   defenderStartPosition() {
     return (
@@ -357,8 +363,8 @@ class Board {
   moveAliens() {
     this.determineDirection();
     this.alienLocations = this.calculateAlienLocations();
-    this.updateCurrentTotalAlienHealth()
-    this.updateScore()
+    this.updateCurrentTotalAlienHealth();
+    this.updateScore();
     for (let i = 0; i < this.squares.length - 1; i++) {
       if (this.squares[i].hasAlien()) {
         let c = this.squares[i].pickCharacterUp();
@@ -375,18 +381,18 @@ class Board {
   }
   calculateCurrentTotalAlienHealth() {
     return this.squares
-                  .filter((square) => square.hasAlien())
-                  .map((square) => square.character.health)
-                  .reduce((a,b) => a + b, 0);
+      .filter((square) => square.hasAlien())
+      .map((square) => square.character.health)
+      .reduce((a, b) => a + b, 0);
   }
   updateCurrentTotalAlienHealth() {
-    this.currentTotalAlienHealth = this.calculateCurrentTotalAlienHealth()
+    this.currentTotalAlienHealth = this.calculateCurrentTotalAlienHealth();
   }
   calculateScore() {
-    return (this.initialTotalAlienHealth - this.currentTotalAlienHealth) * 10
+    return (this.initialTotalAlienHealth - this.currentTotalAlienHealth) * 10;
   }
   updateScore() {
-    this.score = this.calculateScore()
+    this.score = this.calculateScore();
   }
 
   calculateAlienLocations() {
@@ -396,7 +402,7 @@ class Board {
   }
 
   containsAliens() {
-    return this.alienLocations.length != 0 
+    return this.alienLocations.length != 0;
   }
 
   determineDirection() {
@@ -458,17 +464,17 @@ class Board {
     let rocketLocation = this.getIndexOf(rocket);
     let nextRocketLocation = this.getNextIndexOf(rocket);
     this.squareAt(rocketLocation).setCharacter(rocket);
-    
+
     rocketId = setInterval(() => {
       // Sometimes if the interval is just right an alien gets picked up
       // instead of the rocket, the following if statement takes care of
       // this situation by removing the alien that was effectively hit by a rocket
       if (!this.squareAt(rocketLocation).hasRocket()) {
-        this.squareAt(rocketLocation).setEmpty()
-        // Here we clear the interval because otherwise this interval of the rocket will 
-        // keep trying to find that rocket but it is not there 
-        clearInterval(rocketId)
-        return
+        this.squareAt(rocketLocation).setEmpty();
+        // Here we clear the interval because otherwise this interval of the rocket will
+        // keep trying to find that rocket but it is not there
+        clearInterval(rocketId);
+        return;
       }
       let rocketInFlight = this.liftRocketAt(rocketLocation, rocketId);
       this.updateIndexOf(rocket);
@@ -499,12 +505,11 @@ class Board {
 
   updateAlienHitAt(nextRocketLocation) {
     let alienHit = this.squareAt(nextRocketLocation).pickCharacterUp();
-    if (alienHit)
-      this.squareAt(nextRocketLocation).setCharacter(alienHit);
+    if (alienHit) this.squareAt(nextRocketLocation).setCharacter(alienHit);
   }
   reduceAlienHealthByOneAt(nextRocketLocation) {
     this.squareAt(nextRocketLocation).character.health -= 1;
-    this.squareAt(nextRocketLocation).removeDeadAliens()
+    this.squareAt(nextRocketLocation).removeDeadAliens();
   }
   waitAndEraseRocketAt(rocketLocation) {
     setTimeout(() => {
@@ -523,10 +528,10 @@ class Board {
   liftRocketAt(rocketLocation, rocketId) {
     let rocket = this.squareAt(rocketLocation).pickCharacterUp();
     if (!rocket) {
-      clearInterval(rocketId)
+      clearInterval(rocketId);
       throw new Error("couldnt find rocket at location: " + rocketLocation);
     }
-    return rocket
+    return rocket;
   }
   getNextIndexOf(rocket) {
     return rocket.index + this.directionUp();
@@ -584,13 +589,51 @@ function getTheOppositeOf(direction) {
   return direction * -1;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  let alienInvaders = {
-    0: 2, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 180: 1 ,
-    15: 2, 16: 1, 17: 1, 18: 1, 19: 1, 20: 1, 21: 1, 22: 1, 23: 1, 24: 1,
-    30: 2, 31: 2, 32: 2, 33: 2, 34: 2, 35: 2, 36: 2, 37: 2, 38: 2, 39: 2,
-  };
-  setup = new Setup(alienInvaders, []);
+function level(level) {
+  document.querySelector("#game").style.display = "block";
+  document.querySelector("#level-select").style.display = "none";
+  setup = new Setup(level, []);
   game = new Game(setup);
   game.start();
+}
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#game").style.display = "none";
+  document.querySelector("#level-select").style.display = "block";
+  level1Button = document.querySelector("#level1");
+  level2Button = document.querySelector("#level2");
+  level3Button = document.querySelector("#level3");
+  level4Button = document.querySelector("#level4");
+  let level1 = {
+    0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1,
+    15: 1, 16: 1, 17: 1, 18: 1, 19: 1, 20: 1, 21: 1, 22: 1, 23: 1, 24: 1,
+    30: 1, 31: 1, 32: 1, 33: 1, 34: 1, 35: 1, 36: 1, 37: 1, 38: 1, 39: 1,
+  };
+  let level2 =  {
+    0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1,
+    15: 2, 16: 2, 17: 2, 18: 2, 19: 2, 20: 2, 21: 2, 22: 2, 23: 2, 24: 2,
+    30: 1, 31: 1, 32: 1, 33: 1, 34: 1, 35: 1, 36: 1, 37: 1, 38: 1, 39: 1,
+  };
+  let level3 =  {
+    0: 1, 1: 2, 2: 1, 3: 2, 4: 1, 5: 2, 6: 1, 7: 2, 8: 1, 9: 2,
+    15: 2, 16: 1, 17: 2, 18: 1, 19: 2, 20: 1, 21: 2, 22: 1, 23: 2, 24: 1,
+    30: 1, 31: 2, 32: 1, 33: 2, 34: 1, 35: 2, 36: 1, 37: 2, 38: 1, 39: 2,
+  };
+  let level4 =  {
+    0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1,
+    15: 2, 16: 2, 17: 2, 18: 2, 19: 2, 20: 2, 21: 2, 22: 2, 23: 2, 24: 2,
+    30: 2, 31: 2, 32: 2, 33: 2, 34: 2, 35: 2, 36: 2, 37: 2, 38: 2, 39: 2,
+    45: 1, 46: 1, 47: 1, 48: 1, 49: 1, 50: 1, 51: 1, 52: 1, 53: 1, 54: 1,
+  };
+  level1Button.addEventListener("click", () => {
+    level(level1);
+  });
+  level2Button.addEventListener("click", () => {
+    level(level2);
+  });
+  level3Button.addEventListener("click", () => {
+    level(level3);
+  });
+  level4Button.addEventListener("click", () => {
+    level(level4);
+  });
 });
