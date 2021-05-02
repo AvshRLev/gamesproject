a5ddc895b709e7e1e46210f3e962c825416a5af3
AvshRLev <avsha.earth@gmail.com>
2021-05-02 10:31:24 +0300

    two first unit tests

---
+++ __test__/defender.test.js
@@ -0,0 +1,8 @@
+const Defender = require("../static/defender");
+
+describe("Defender is the spaceship that shoots the aliens", () => {
+  test("get its css class", () => {
+    defender = new Defender('defender', 15);
+    expect(defender.getCssClass()).toEqual('defender');
+  });
+});
