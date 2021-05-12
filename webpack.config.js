ae173d8aa82bd48fe45eff36d86720478af21fab
AvshRLev <avsha.earth@gmail.com>
2021-05-12 15:04:02 +0300

    Edit webpack.config.js to be relative path

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
+  mode: "development",
 
-  mode: 'development',
-
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
 
   entry: {
-    bundle: 'C:/Users/User/Documents/Cs50/project/project/static/proto.js',
-    
+    bundle: "./static/proto.js",
   },
-  
+
   output: {
-    path: path.resolve(__dirname, "C:/Users/User/Documents/Cs50/project/project/static/dist"), // Should be in STATICFILES_DIRS
+    path: path.resolve(__dirname, "./static/dist"), // Should be in STATICFILES_DIRS
     publicPath: "/static/", // Should match Django STATIC_URL
     filename: "[name].js", // No filename hashing, Django takes care of this
     chunkFilename: "[id]-[chunkhash].js", // DO have Webpack hash chunk filename, see below
-    },
-      
-    devServer: {
-        writeToDisk: true, // Write files to disk in dev mode, so Django can serve the assets
-    }
+  },
+
+  devServer: {
+    writeToDisk: true, // Write files to disk in dev mode, so Django can serve the assets
+  },
 };
