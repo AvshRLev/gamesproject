2147e49f0f7a3fd9842aecb922b86e18244c586d
AvshRLev <avsha.earth@gmail.com>
2021-05-20 22:21:12 +0300

    Github sqlite db

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,6 +86,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    }
 
 AUTH_USER_MODEL = 'games.User'
 
