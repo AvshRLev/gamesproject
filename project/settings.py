0a00bd0847396de2175ab19f554c6c06f5b27c55
AvshRLev <avsha.earth@gmail.com>
2021-05-20 18:00:57 +0300

    Update settings.py to work with postgres

--- project/settings.py
+++ project/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spacedb',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
