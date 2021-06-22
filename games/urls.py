ec2e54489661abfab42a9a2470455820cdd83b39
AvshRLev <avsha.earth@gmail.com>
2021-06-22 16:53:49 +0300

    Update urls.py, settings.py

--- games/urls.py
+++ games/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # path("", views.index, name="index"),
     path("", views.space_invaders, name="space-invaders"),
+    path("index", views.space_invaders, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
